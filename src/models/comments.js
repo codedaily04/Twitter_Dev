5be4366509f5b8077cb1217b9007ed4035f46341
Abhishek Nishad <codedaily04@gmail.com>
2026-06-03 00:16:09 +0530

    Modify Comment Model

--- src/models/comments.js
+++ src/models/comments.js
@@ -5,9 +5,21 @@ const CommentSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    Email:{
-        type:String
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
     },
+    onModel:{
+        type:String,
+        required:true,
+        enum:['Tweet','Comment']
+    },
+    commentable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'
+    }
 },{timestamps:true});
 
 const Comment=mongoose.model('Comment',CommentSchema);
